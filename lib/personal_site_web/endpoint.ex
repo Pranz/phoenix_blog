5a46dd88ce77ecb9529429439d340853ed88ee39
Jesper Fridefors <jesper.fridefors@gmail.com>
2018-08-11 22:18:30 +0200

    allow letsencrypt to access challenges

--- lib/personal_site_web/endpoint.ex
+++ lib/personal_site_web/endpoint.ex
@@ -9,7 +9,7 @@ defmodule PersonalSiteWeb.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :personal_site, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js favicon.ico robots.txt .well-known)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
